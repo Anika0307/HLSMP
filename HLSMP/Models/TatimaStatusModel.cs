92238d5268f8760bc469746e15d7c11243d70d9b
Anika0307 <anikatakkar1910@gmail.com>
2025-05-21 14:13:18 +0530

    GisLab Done

---
+++ HLSMP/Models/TatimaStatusModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HLSMP.Models
+{
+    public class TatimaStatusModel
+    {
+        public int TatimaID { get; set; }
+        public string Status { get; set; }
+    }
+}
--- HLSMP/ViewModel/GISViewModel.cs
+++ HLSMP/ViewModel/GISViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using HLSMP.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
 
@@ -13,8 +14,11 @@ namespace HLSMP.ViewModel
         public string VillageStage { get; set; }
 
         public List<SelectListItem> DistrictList { get; set; } = new();
+        [ValidateNever]
         public List<SelectListItem> TehsilList { get; set; }
+        [ValidateNever]
         public List<SelectListItem> VillageList { get; set; }
+        [ValidateNever]
         public List<SelectListItem> VillageStageList { get; set; }
 
         public bool? IsWorkDone { get; set; }
@@ -25,6 +29,15 @@ namespace HLSMP.ViewModel
         [Required]
         [Display(Name = "Upload Document")]
         public IFormFile UploadedFile { get; set; }
+
+        [Required]
+        public int TotalTatima { get; set; }
+
+        [Required]
+        public int Completed { get; set; }
+
+        [Required]
+        public int Pending { get; set; }
 
     }
 }
